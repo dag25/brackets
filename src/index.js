5241e1a222cad690b39a6326ee8b704a5f2a2b5f
dag <45577604+dag25@users.noreply.github.com>
2024-09-13 13:07:04 +0400

    feat: first attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const map = {
+		')': '(',
+		']': '[',
+		'}': '{',
+	};
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+		// check for opening bracket
+		if (Object.values(map).includes(str[i])) {
+			stack.push(str[i]);
+		} else if (stack[stack.length - 1] === map[str[i]]) {
+			stack.pop();
+		} else {
+			return false;
+		}
+	}
+
+	return stack.length === 0 ? true : false;
 }
