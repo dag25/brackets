a903ca51b22747b9005f285047b075d9949055c0
dag <45577604+dag25@users.noreply.github.com>
2024-09-14 12:13:32 +0400

    feat: fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,67 @@
 module.exports = function check(str, bracketsConfig) {
-  const map = {
-		')': '(',
-		']': '[',
-		'}': '{',
+  let stackArray = [];
+	let openBracketsArray = [];
+	let closeBracketsArray = [];
+
+	str = str.split('');
+
+	const settingArrays = bracketsConfig => {
+		for (let i = 0; bracketsConfig.length > i; i++) {
+			openBracketsArray.push(bracketsConfig[i][0]);
+		}
 	};
-  let stack = [];
-  for (let i = 0; i < str.length; i++) {
-		// check for opening bracket
-		if (Object.values(map).includes(str[i])) {
-			stack.push(str[i]);
-		} else if (stack[stack.length - 1] === map[str[i]]) {
-			stack.pop();
+
+	const compareOpenClose = (char, bracketsConfig) => {
+		for (let i = 0; bracketsConfig.length > i; i++) {
+			if (bracketsConfig[i][0] === char && bracketsConfig[i][1] === char) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+	const isOpenBr = (char, open) => {
+		if (open.indexOf(char) > -1) {
+			return true;
 		} else {
 			return false;
 		}
-	}
+	};
 
-	return stack.length === 0 ? true : false;
+	const matching = (topStack, char, bracketsConfig) => {
+		for (let i = 0; bracketsConfig.length > i; i++) {
+			if (bracketsConfig[i][0] === topStack && bracketsConfig[i][1] === char) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+
+	settingArrays(bracketsConfig);
+	for (let i = 0; str.length > i; i++) {
+		if (compareOpenClose(str[i], bracketsConfig)) {
+			if (stackArray[stackArray.length - 1] === str[i]) {
+				stackArray.pop();
+			} else {
+				stackArray.push(str[i]);
+			}
+		} else {
+			if (isOpenBr(str[i], openBracketsArray)) {
+				stackArray.push(str[i]);
+			} else {
+				if (stackArray.length === 0) {
+					return false;
+				}
+				if (!matching(stackArray.pop(), str[i], bracketsConfig)) {
+					return false;
+				}
+			}
+		}
+	}
+	if (stackArray.length === 0) {
+		return true;
+	} else {
+		return false;
+	}
 }
